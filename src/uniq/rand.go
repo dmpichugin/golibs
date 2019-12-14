57da506854c26938911a224e6e6bf335f83e9df4
Pichugin Dmitry <pichugin.dmitry.v@gmail.com>
2019-12-14 10:01:28 +0300

    gofmt

--- src/uniq/rand.go
+++ src/uniq/rand.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-var(
+var (
 	seed = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
@@ -18,18 +18,17 @@ func Int63n(n int64) int64 {
 
 	return seed.Int63n(n)
 }
-
 func Int31() int32 {
 
 	return seed.Int31()
 }
 
-func Int31n(n int32) int32  {
+func Int31n(n int32) int32 {
 
 	return seed.Int31n(n)
 }
 
-func Int () int {
+func Int() int {
 
 	return seed.Int()
 }
