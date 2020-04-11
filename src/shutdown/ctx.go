585cebdd0e658f49e5864bc89652751752081a05
Dmitry Pichugin <pichugin.dmitry.v@gmail.com>
2020-04-11 15:48:49 +0300

    Add shutdown package

---
+++ src/shutdown/ctx.go
@@ -0,0 +1,46 @@
+package shutdown
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+var (
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	once sync.Once
+)
+
+func Ctx() context.Context {
+
+	once.Do(func() {
+		initCtx()
+		go listener()
+	})
+
+	return ctx
+}
+
+func listener() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-ctx.Done():
+	case <-signalChan:
+	}
+
+	cancel()
+}
+
+func initCtx() {
+	ctx, cancel = context.WithCancel(context.Background())
+}
+
+func Force() {
+	cancel()
+}
