95c033f9ed7d9ed1f427cd29f238b7e18bd376ad
Dmitry Pichugin <pichugin.dmitry.v@gmail.com>
2020-06-07 13:25:41 +0300

    Add uuid regexp test
    Signed-off-by: Dmitry Pichugin <pichugin.dmitry.v@gmail.com>

--- src/uniq/uuid_test.go
+++ src/uniq/uuid_test.go
@@ -1,8 +1,20 @@
 package uniq
 
 import (
+	"regexp"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestUUIDRegexp(t *testing.T) {
+	re, err := regexp.Compile("^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$")
+	assert.NoError(t, err, "compile regular")
+	if err != nil {
+		return
+	}
+	assert.True(t, re.MatchString(UUID()))
+}
 
 func BenchmarkUUIDv4(b *testing.B) {
 	for i := 0; i < b.N; i++ {
